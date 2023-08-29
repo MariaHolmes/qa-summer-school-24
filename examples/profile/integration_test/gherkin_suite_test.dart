cfd8ab5c8a206317c077762e74444a2f58a15ed7
Maria Holms <maria.l.a@yandex.ru>
2023-08-29 18:49:41 +0300

    Добавляем что успели

---
+++ examples/profile/integration_test/gherkin_suite_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+part 'gherkin_suite_test.g.dart';
+
+@GherkinTestSuite()
+void main () {
+
+}
